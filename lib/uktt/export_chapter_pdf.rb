a1145ef8658e290add6b802926ccabb1e659acaf
Christopher Unger <mcu@mac.com>
2019-06-24 12:09:46 -0400

    Add more space to footnotes number column,

--- lib/uktt/export_chapter_pdf.rb
+++ lib/uktt/export_chapter_pdf.rb
@@ -585,7 +585,7 @@ class ExportChapterPdf
       borders: []
     }
     table_opts = {
-      column_widths: [@indent_amount],
+      column_widths: [@indent_amount + 3],
       width: @printable_width,
       cell_style: cell_style
     }
