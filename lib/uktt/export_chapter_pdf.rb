2cac983d871afee212a738c69251f28d35535ecf
Alexey Klimuk <alexey.klimuk@bitzesty.com>
2020-01-22 13:34:51 +0100

    Tariff PDF: rename headings for chapter notes

--- lib/uktt/export_chapter_pdf.rb
+++ lib/uktt/export_chapter_pdf.rb
@@ -326,7 +326,7 @@ class ExportChapterPdf
         text("<b><font size='#{@base_table_font_size * 1.5}'>Chapter #{chapter.data.attributes.goods_nomenclature_item_id[0..1].gsub(/^0/, '')}\n#{@chapter.data.attributes.formatted_description}</font></b>", opts)
         move_down(@base_table_font_size * 1.5)
 
-        text('<b>Note</b>', opts.merge(size: 9))
+        text('<b>Chapter notes</b>', opts.merge(size: 9))
         notes.split(/\* /).each do |note|
           text_indent(note, opts.merge(size: 9))
         end
@@ -334,7 +334,7 @@ class ExportChapterPdf
         move_down(@base_table_font_size)
 
         if additional_notes
-          text('<b>Additional Notes</b>', opts)
+          text('<b>Subheading notes</b>', opts)
           move_down(@base_table_font_size / 2)
           additional_notes && additional_notes.split(/\* /).each do |note|
             text_indent(note, opts)
@@ -343,7 +343,7 @@ class ExportChapterPdf
         end
 
         everything_else.each do |nn|
-          text('<b>Notes</b>', opts)
+          text('<b>Additional notes</b>', opts)
           move_down(@base_table_font_size / 2)
           nn.to_s.split(/\* /).each do |note|
             text_indent(note, opts)
